15af7e24808cb831bce5a7bb29a3f3e3a15b18c6
Sharonk17 <ksharonwork@gmail.com>
2025-08-06 14:56:39 +0530

    Initial commit

--- src/day1/LoopingStatement.java
+++ src/day1/LoopingStatement.java
@@ -4,7 +4,10 @@ public class LoopingStatement {
 	
 	
 	public static void main(String[] args) {
-		System.out.println();
+		for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
 	}
-
 }
